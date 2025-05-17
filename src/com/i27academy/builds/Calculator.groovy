1acfb61572a107f001b728f6c1a168ae181863b6
Ikkurthi24568 <Ikkurthi24568@gmail.com>
2025-05-17 10:34:08 +0530

    Update Calculator.groovy

--- src/com/i27academy/builds/Calculator.groovy
+++ src/com/i27academy/builds/Calculator.groovy
@@ -1,18 +1,15 @@
-package.com.i27academy.builds;
+package com.i27academy.builds;
 //all the methods
 class Calculator{
     def jenkins
-
 Calculator(jenkins){
     this.jenkins = jenkins
 }
-
 
 //Adition Method
 def add(firstNumber,secondNumber){
     return firstNumber + secondNumber
 }
-
 // Multiplication Method
 def multiply(firstNumber,secondNumber){
     return firstNumber * secondNumber
